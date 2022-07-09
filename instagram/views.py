961b7450142c58228dc6c5c2ec02e07d95632506
j-lee <kerneltrek@gmail.com>
2022-07-09 18:10:06 +0900

    UserPage Add Function - Follow / Unfollow & MainPage Add Standard - Post is GTE (now - 3day)

--- instagram/views.py
+++ instagram/views.py
@@ -1,19 +1,28 @@
+from datetime import timedelta
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 # Create your views here.
+from django.utils import timezone
+
 from instagram.forms import PostForm
 from .models import Tag, Post
 
 @login_required
 def index(request):
     #작성자가 본인이나, 팔로워인 경우만 가져옴
+
+    timesince = timezone.now() - timedelta(days=3)
     post_list = Post.objects.all()\
                 .filter(
                     Q(author=request.user) |
                     Q(author__in=request.user.following_set.all())
+                )\
+                .filter(
+                    created_at__gte=timesince # less then equal
                 )
     suggested_user_list = get_user_model().objects.all()\
             .exclude(pk=request.user.pk)\
@@ -50,9 +59,15 @@ def user_page(request, username):
     page_user = get_object_or_404(get_user_model(), username=username, is_active=True)
     post_list = Post.objects.filter(author=page_user)
     post_list_count = post_list.count() # db 에 카운트를 던져서 얻어옴
+    print(request.user.is_authenticated)
+    if request.user.is_authenticated:
+        is_follow = request.user.following_set.filter(pk=page_user.pk).exists()
+    else:
+        is_follow = False
     # len(post_list) # 메모리에 로드한 후 개수를 세는 방식
     return render(request, "instagram/user_page.html", {
         "page_user": page_user,
         "post_list": post_list,
         "post_list_count": post_list_count,
+        "is_follow": is_follow
     })
